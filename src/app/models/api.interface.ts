3ae5995c70d87573b980862b9bee1f0d7fdd6012
Igor <igor.aniskevich@innowise.com>
2025-05-16 17:43:59 +0200

    refactor: standardize formatting in ApiInterface and TvShow models

--- src/app/models/api.interface.ts
+++ src/app/models/api.interface.ts
@@ -1,18 +1,20 @@
 export interface ApiInterface {
-  total: number
-  page: number
-  pages: number
-  tv_shows: TvShow[]
+  total: number;
+  page: number;
+  pages: number;
+  tv_shows: TvShow[];
 }
 
 export interface TvShow {
-  id: number
-  name: string
-  permalink: string
-  start_date: string
-  end_date?: string
-  country: string
-  network: string
-  status: string
-  image_thumbnail_path: string
+  id: number;
+  name: string;
+  permalink: string;
+  start_date: string;
+  end_date?: string;
+  country: string;
+  network: string;
+  status: Status;
+  image_thumbnail_path: string;
 }
+
+export type Status = 'Canceled/Ended' | 'Ended' | 'Running' | 'New Series' | 'To Be Determined';
