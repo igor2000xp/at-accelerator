287f5e4803429ceb42523e02661e9ddc64bad186
Igor <igor.aniskevich@innowise.com>
2025-05-18 19:06:32 +0200

    refactor: remove unused error handling code in ApiService - Eliminated catchError and handleError methods to simplify the getSearchesResults function.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { ApiInterface, TvShow } from './../models/api.interface';
-import { catchError, Observable, map, throwError, BehaviorSubject } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, map, BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -16,22 +16,24 @@ export class ApiService {
 
   getSearchesResults(searchString = '') {
     this.$searchObserver = this.http.get<ApiInterface>(`search?q=${searchString}&page=1 `).pipe(
-      map((response: ApiInterface) => response.tv_shows),
-      catchError(err => this.handleError(err))
+      map((response: ApiInterface) => response.tv_shows)
+
+      // Note: The browser would log pretty much like that by default, so don't really need that code.
+      // catchError(err => this.handleError(err))
     );
     return this.$searchObserver;
   }
 
-  private handleError(error: HttpErrorResponse): Observable<never> {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
-  }
+  // private handleError(error: HttpErrorResponse): Observable<never> {
+  //   if (error.error instanceof ErrorEvent) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error.message);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+  //   }
+  //   // Return an observable with a user-facing error message.
+  //   return throwError(() => new Error('Something bad happened; please try again later.'));
+  // }
 }
