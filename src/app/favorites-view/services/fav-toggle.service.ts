7fa9434bcb7fcc5527278610ac6f8c99b01dfb97
Igor <igor.aniskevich@innowise.com>
2025-05-18 21:20:54 +0200

    feat: implement favorite management methods in FavToggleService

--- src/app/favorites-view/services/fav-toggle.service.ts
+++ src/app/favorites-view/services/fav-toggle.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
+import { TvShow } from 'src/app/models/api-interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavToggleService {
+  private localStorageService = inject(LocalStorageService);
 
-  constructor() { }
+  // constructor() { }
+
+  getLocalStorageFavorites(): TvShow[] {
+    return this.localStorageService.getFavoritesStore<TvShow[]>('favorites') || [];
+  }
+
+  addToFavorites(show: TvShow): void {
+    const favorites = this.getLocalStorageFavorites();
+    favorites.push(show);
+    this.localStorageService.setFavoritesStore('favorites', favorites);
+  }
+
+  removeFromFavorites(show: TvShow): void {
+    const newFavorites = this.getLocalStorageFavorites().filter(item => item.id !== show.id);
+    this.localStorageService.setFavoritesStore('favorites', newFavorites);
+  }
+
+  emptyFavorites(): void {
+    this.localStorageService.setFavoritesStore('favorites', []);
+  }
 }
