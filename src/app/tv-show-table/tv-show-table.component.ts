ebe8436b68befa8191ff9fd8458d2b0dc4f41f29
Igor <igor.aniskevich@innowise.com>
2025-05-18 11:13:53 +0200

    feat: integrating user input and displaying results in TV show table

--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TvShow } from '../models/api-interface';
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './tv-show-table.component.html',
-  styleUrls: ['./tv-show-table.component.css']
+  styleUrls: ['./tv-show-table.component.css'],
 })
 export class TvShowTableComponent {
-
+  @Input() data!: Signal<TvShow[]>;
 }
