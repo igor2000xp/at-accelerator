d5eee1e842d529728116dd18a704c7cea677a33f
Igor <igor.aniskevich@innowise.com>
2025-05-18 20:46:41 +0200

    refactor: remove unused constructor from LocalStorageService

--- src/app/favorites-view/services/local-storage.service.ts
+++ src/app/favorites-view/services/local-storage.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LocalStorageService {
-  // constructor() { }
   setFavoritesStore<T>(key: string, obj: T) {
     localStorage.setItem(key, JSON.stringify(obj));
   }
